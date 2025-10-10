7495eb32998f3844ee0320d7bdea50914033fc4c
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-10-10 22:10:13 +0200

    clean up inference

--- 01_inference.py
+++ 01_inference.py
@@ -2,70 +2,81 @@ import torchio as tio
 import torch
 from pathlib import Path
 
-from data import get_subject_dict, SUVLogCompress
 
-count_reduction_factor = 10
-patch_size = 64
-patch_overlap = 32
-batch_size = 20
-target_voxsize_mm = 1.65
+def patch_inference(
+    subject: tio.Subject,
+    scripted_model_path: Path,
+    count_reduction_factor=10,
+    patch_size=64,
+    patch_overlap=32,
+    batch_size=20,
+):
 
-s_dirs = sorted(
-    [
-        x
-        for x in Path(
-            "/uz/data/Admin/ngeworkingresearch/schramm_lab/data/2025_ud_pet_challenge/nifti_out"
-        ).iterdir()
-        if x.is_dir()
-    ]
-)
+    grid_sampler = tio.inference.GridSampler(
+        subject,
+        patch_size,
+        patch_overlap,
+    )
 
-s_dir = s_dirs[421]
+    patch_loader = tio.SubjectsLoader(grid_sampler, batch_size=batch_size)
+    aggregator = tio.inference.GridAggregator(grid_sampler)
 
-# %%
-transform_list = [tio.transforms.ToCanonical()]
-if target_voxsize_mm is not None:
-    transform_list += [tio.Resample(target=target_voxsize_mm)]
-transform_list += [SUVLogCompress()]
-transform = tio.Compose(transform_list)
+    # load the model from torch script
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = torch.jit.load(scripted_model_path, map_location=device)
 
-subject = transform(tio.Subject(get_subject_dict(s_dir, crfs=[count_reduction_factor])))
+    model.eval()
+    with torch.no_grad():
+        for patches_batch in patch_loader:
+            input_tensor = patches_batch[f"{count_reduction_factor}"][tio.DATA].to(
+                device
+            )
+            locations = patches_batch[tio.LOCATION].to(device)
+            outputs = model(input_tensor)
+            aggregator.add_batch(outputs, locations)
 
-grid_sampler = tio.inference.GridSampler(
-    subject,
-    patch_size,
-    patch_overlap,
-)
+    return aggregator.get_output_tensor()
 
-patch_loader = tio.SubjectsLoader(grid_sampler, batch_size=batch_size)
-aggregator = tio.inference.GridAggregator(grid_sampler)
-
-# load the model from torch script
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = torch.jit.load("model_0001_scripted.pt", map_location=device)
-
-model.eval()
-with torch.no_grad():
-    for patches_batch in patch_loader:
-        input_tensor = patches_batch[f"crf{count_reduction_factor}"][tio.DATA].to(
-            device
-        )
-        locations = patches_batch[tio.LOCATION].to(device)
-        outputs = model(input_tensor)
-        aggregator.add_batch(outputs, locations)
-
-output_tensor = aggregator.get_output_tensor()
 
 # %%
-import numpy as np
-import pymirc.viewer as pv
 
-o = output_tensor.cpu().numpy().squeeze()
-ref = subject["ref"].numpy().squeeze()
-i = subject["crf10"].numpy().squeeze()
+if __name__ == "__main__":
+    import numpy as np
+    import pymirc.viewer as pv
+    from data import get_subject_dict, SUVLogCompress
 
-o2 = np.exp(o) - 1
-i2 = np.exp(i) - 1
-ref2 = np.exp(ref) - 1
+    model_path = Path("run_20251010_180549/model_0020_scripted.pt")
+    target_voxsize_mm = 1.65
+    count_reduction_factor = 10
 
-vi = pv.ThreeAxisViewer([i2, o2, ref2], imshow_kwargs=dict(vmin=0, vmax=5))
+    m_path = Path(
+        "/uz/data/Admin/ngeworkingresearch/schramm_lab/data/2025_ud_pet_challenge/nifti_out"
+    )
+    s_dirs = sorted([x for x in m_path.iterdir() if x.is_dir()])
+    s_dir = s_dirs[101]
+
+    transform = tio.Compose(
+        [
+            tio.transforms.ToCanonical(),
+            tio.Resample(target=target_voxsize_mm),
+        ]
+    )
+
+    subject = transform(
+        tio.Subject(get_subject_dict(s_dir, crfs=[str(count_reduction_factor), "ref"]))
+    )
+
+    output_tensor = patch_inference(
+        subject,
+        model_path,
+        count_reduction_factor=count_reduction_factor,
+    )
+    o = output_tensor.cpu().numpy().squeeze()
+    ref = subject["ref"].numpy().squeeze()
+    i = subject[f"{count_reduction_factor}"].numpy().squeeze()
+
+    o2 = np.exp(o) - 1
+    i2 = np.exp(i) - 1
+    ref2 = np.exp(ref) - 1
+
+    vi = pv.ThreeAxisViewer([i2, o2, ref2], imshow_kwargs=dict(vmin=0, vmax=5))
