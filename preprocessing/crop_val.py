39d34a055b521922d0f2d1822f860c7e7f1a58a3
Georg Schramm (Windows) <georg.schramm@kuleuven.be>
2025-10-15 13:55:52 +0200

    write compressed cropped niftis

--- preprocessing/crop_val.py
+++ preprocessing/crop_val.py
@@ -3,36 +3,36 @@ import numpy as np
 from pathlib import Path
 from scipy.ndimage import find_objects
 
-mdir = Path(
-    "/uz/data/Admin/ngeworkingresearch/schramm_lab/data/2025_ud_pet_challenge/nifti_out"
-)
+mdir = Path("/tmp/2025_ud_pet_challenge/nifti_out")
 
 with open("../val.txt", "r") as f:
     validation_s_dirs = [mdir / Path(line.strip()) for line in f.readlines()]
 
 for s_dir in validation_s_dirs:
-    print(s_dir.name)
-
-    ref_nii = nib.load(s_dir / "ref" / "resampled_1.65.nii.gz")
-    ref_vol = ref_nii.get_fdata().astype(np.float32)
-
-    # find the bounding box
-    bbox = find_objects((ref_vol > 0.1).astype(int))[0]
-
-    # save ref volume cropped to bbox
-    # Adjust the affine to account for cropping
-    start = [bbox[i].start for i in range(3)]
-    new_affine = ref_nii.affine.copy()
-    new_affine[:3, 3] += ref_nii.affine[:3, :3] @ start
-    new_affine = new_affine.astype(np.float32)
-
     orfile = s_dir / "ref" / "resampled_1.65_cropped.nii.gz"
-    nib.save(nib.Nifti1Image(ref_vol[bbox], new_affine), orfile)
 
-    for d in ["100", "50", "20", "10", "4"]:
-        print(d)
-        odfile = s_dir / d / "resampled_1.65_cropped.nii"
-        dfile = s_dir / d / "resampled_1.65.nii.gz"
-        nii = nib.load(dfile)
-        vol = nii.get_fdata().astype(np.float32)
-        nib.save(nib.Nifti1Image(vol[bbox], new_affine), odfile)
+    if not orfile.exists():
+        print(s_dir.name)
+        ref_nii = nib.load(s_dir / "ref" / "resampled_1.65.nii.gz")
+        ref_vol = ref_nii.get_fdata().astype(np.float32)
+
+        # find the bounding box
+        bbox = find_objects((ref_vol > 0.1).astype(int))[0]
+
+        # save ref volume cropped to bbox
+        # Adjust the affine to account for cropping
+        start = [bbox[i].start for i in range(3)]
+        new_affine = ref_nii.affine.copy()
+        new_affine[:3, 3] += ref_nii.affine[:3, :3] @ start
+        new_affine = new_affine.astype(np.float32)
+
+        nib.save(nib.Nifti1Image(ref_vol[bbox], new_affine), orfile)
+
+        for d in ["100", "50", "20", "10", "4"]:
+            print(d)
+            odfile = s_dir / d / "resampled_1.65_cropped.nii.gz"
+            if not odfile.exists():
+                dfile = s_dir / d / "resampled_1.65.nii.gz"
+                nii = nib.load(dfile)
+                vol = nii.get_fdata().astype(np.float32)
+                nib.save(nib.Nifti1Image(vol[bbox], new_affine), odfile)
